e093577f6a795fd6349efa9d220b6de9f3f87eee
Bryan Fink <bryan.fink@gmail.com>
2017-02-17 20:03:43 -0800

    A little doc and some regular posting to helium

--- ms5607.lua
+++ ms5607.lua
@@ -35,10 +35,13 @@ MS5607 = {
    CMD_ADC_READ = 0x00,    -- read the value
 }
 
+-- Reset the part. Use after power on, before starting a conversion.
 function ms5607reset(addr)
    i2c.txn(i2c.tx(addr, MS5607.CMD_RESET))
 end
 
+-- Read the ith coefficient.
+-- Returns a 16-bit unsigned integer on success, nil on failure.
 function read_coefficient(addr, i)
    local command = MS5607.CMD_PROM_READ |
       (MS5607.PROM_ADDR_MASK & (i * 2))
@@ -49,6 +52,8 @@ function read_coefficient(addr, i)
    end
 end
 
+-- Read all coefficients.
+-- Returns a Table with entry keys 0-7.
 function read_coefficients(addr)
    local coefficients = {}
    
@@ -61,21 +66,30 @@ function read_coefficients(addr)
    return coefficients
 end
 
+-- Tell the part to read its ADC and get its value ready to be read later.
+-- Parameter 'sample' should be CONVERT_D1 (pressure) or CONVER_D2 (temp).
+-- Parameter 'resolution' should be one of OSR_x
+-- Wait for CONV_TIME_x after this function before calling read_adc.
 function start_conversion(addr, sample, resolution)
    local cmd = MS5607.CMD_CONVERT | sample, resolution
    i2c.txn(i2c.tx(addr, cmd))
 end
 
+-- Read the last converted ADC value from the part.
+-- Parameter 'resolution' should be one of OSR_x
+-- Return value is an unsigned 24-bit integer.
 function read_adc(addr, resolution)
    local status, buffer = i2c.txn(i2c.tx(addr, MS5607.CMD_ADC_READ),
                                   i2c.rx(addr, 3))
-
    if status then
       -- > big endian
       return string.unpack(">I3", buffer)
    end
 end
 
+-- Read both temperature and pressure (currently at resolution 4096)
+-- Parameter 'coefficients' should be the table returned from read_coefficients
+-- Returns 'temp, pressure' in /100s degrees C and /100s millibars
 function get_reading(addr, coefficients)
    start_conversion(addr, MS5607.CONVERT_D1, MS5607.OSR_4096)
    he.wait{time=MS5607.CONV_TIME_4096 + he.now()}
@@ -103,6 +117,7 @@ print(string.format("Using addr 0x%X", addr))
 
 he.power_set(true)
 
+-- coefficients are PROM, so we only need to read them once
 ms5607reset(addr)
 coefficients = read_coefficients(addr)
 
@@ -110,8 +125,42 @@ for i=1,6,1 do
    print("Coefficient["..i.."] = "..coefficients[i])
 end
 
-temp, pres = get_reading(addr, coefficients)
+-- prepare for reporting & sleeping
+now = he.now()
 
-print("Temperature: "..temp.." Pressure: "..pres)
+-- number of samples to average to reduce noise
+samples = 10
 
-he.power_set(false)
+while true do
+   local temp = 0
+   local press = 0
+   local failures = 0
+
+   for i=1,10,1 do
+      lt, lp = get_reading(addr, coefficients)
+      if lt and lp then
+         temp = temp + lt
+         press = press + lp
+      else
+         failures = failures+1
+      end
+   end
+
+   temp = temp / (samples-failures)
+   press = press / (samples-failures)
+   quality = (samples-failures) / samples
+
+   -- report in degrees C and millibars
+   he.send("ti", now, "f", temp/100)
+   he.send("pi", now, "f", press/100)
+
+   print("Temperature: "..(temp/100).."C Pressure: "..(press/100).."mbar")
+
+   -- power down while we wait until next reading
+   he.power_set(false)
+   now = he.wait({time=60*1000 + now})
+
+   -- doing this here just makes the top of the loop cleaner
+   he.power_set(true)
+   ms5607reset(addr)
+end
